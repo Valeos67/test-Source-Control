ba9705b6a58e53d6ea9a32a9d8e922e676578448
Valentin SCHERER <valentinscherer@mbp-de-valentin.fritz.box>
2018-08-15 13:45:26 +0200

    Ajout class Test

---
+++ Test Source Control/Test.swift
@@ -0,0 +1,18 @@
+//
+//  Test.swift
+//  Test Source Control
+//
+//  Created by Valentin SCHERER on 15/08/2018.
+//  Copyright © 2018 Valentin SCHERER. All rights reserved.
+//
+
+import Foundation
+
+class Test {
+   
+    func salut() {
+        print("Salut les codabeilles")
+    }
+}
+
+
