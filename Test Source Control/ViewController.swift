a84cd701d4952fd6fd826cfabaea710488b96504
Valentin SCHERER <valentinscherer@mbp-de-valentin.fritz.box>
2018-08-15 13:35:37 +0200

    Second commit

--- Test Source Control/ViewController.swift
+++ Test Source Control/ViewController.swift
@@ -13,7 +13,7 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        print("Premier Commit")
+        print("2eme Commit")
     }
 
     override func didReceiveMemoryWarning() {
